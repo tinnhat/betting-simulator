bd2e9f2834bbe792d3592d4894ee6a2abeb2f5e1
Nhat Tin <tinnhat0412@gmail.com>
2024-10-13 16:15:49 +0700

    update file route bet test

--- src/app/api/bet/route.ts
+++ src/app/api/bet/route.ts
@@ -79,3 +79,16 @@
 //     return NextResponse.json({ error }, { status: 500 });
 //   }
 // }
+import { sql, db } from '@vercel/postgres'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const result = await sql`SELECT * FROM "users"`;
+    // const result = await sql`UPDATE "Users" SET money = 2000000`
+    return NextResponse.json({ result: result.rows }, { status: 200 })
+  } catch (error) {
+    console.error('Error connecting to database:', error)
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
